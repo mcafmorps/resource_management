ba472ef1fd5b6d8ada642f0ee5975fa7aa78bffa
mcafmorps <mcafmorps@gmail.com>
2024-06-12 08:42:35 -0700

    change manifest summary

--- resource_reservation/__manifest__.py
+++ resource_reservation/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Resource Reservation",
-    'summary': "Resource reservation module",
+    'summary': "Resource res module",
     'version': '16.0.1.3.3',
     'website': "https://mcaf.nb.ca/en/",
     'author': "MCAF",
